4121d65c45764f9c85c567c0a79fc7bfff407dcf
proj-athena <proj.athena@gmail.com>
2022-09-29 19:22:25 +0530

    layout updated

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,11 +6,10 @@ const Layout = ({ children }) => {
         <>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home">GENSPEAKS</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="/dashboard" className="active">Dashboard</Nav.Link>
+                        <Nav.Link href="/admin">Admin</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
